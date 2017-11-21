20d65a2822d9f472b8798f687cb010ece1662715
HJBowers <HJBowers@gmail.com>
2017-11-21 12:37:14 -0800

    Audio-Toolkit is working

--- App.js
+++ App.js
@@ -4,10 +4,16 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
+  TouchableHighlight
 } from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
 import Aubergine from './mapStyles/aubergine.json';
+import {
+    Player,
+    Recorder,
+    MediaStates
+} from 'react-native-audio-toolkit';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -26,53 +32,76 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class App extends Component<{}> {
 
-    constructor() {
-      super();
-      this.watchID = null;
+  constructor() {
+    super();
+    this.watchID = null;
 
-      this.state = {
-        region: {
-          latitude: LATITUDE,
-          longitude: LONGITUDE,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        }
-      };
-    }
+    this.state = {
+      disabled: false,
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
 
-    componentDidMount() {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({
-            region: {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }
-          });
-        },
-      (error) => console.log(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-      );
-      this.watchID = navigator.geolocation.watchPosition(
-        position => {
-          this.setState({
-            region: {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }
-          });
-        }
-      );
-    }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }
+    );
+  }
 
-    componentWillUnmount() {
-      navigator.geolocation.clearWatch(this.watchID);
-    }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
+  _onPress() {
+    // Disable button while recording and playing back
+    this.setState({disabled: true});
+
+    // Start recording
+    let rec = new Recorder("filename.mp4").record();
+
+    // Stop recording after approximately 3 seconds
+    setTimeout(() => {
+      rec.stop((err) => {
+        // NOTE: In a real situation, handle possible errors here
+
+        // Play the file after recording has stopped
+        new Player("filename.mp4")
+        .play()
+        .on('ended', () => {
+          // Enable button again after playback finishes
+          this.setState({disabled: false});
+        });
+      });
+    }, 3000);
+  }
 
   render() {
     return (
@@ -101,6 +130,11 @@ export default class App extends Component<{}> {
             coordinate={ this.state.region }
           />
         </MapView>
+        <TouchableHighlight disabled={this.state.disabled} onPress={() => this._onPress()}>
+          <Text>
+            Press me!
+          </Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -108,7 +142,7 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
-    height: '50%',
+    height: '40%',
     width: '100%',
   }
 });
