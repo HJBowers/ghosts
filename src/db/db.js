25988b84f2f9824e64dea9be4c07da1ffe383b3d
leikkisa <sallymaki@gmail.com>
2017-11-21 16:34:10 -0800

    Update db url for heroku

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,6 @@
 const { Client } = require('pg')
 
-const db = new Client({
-  
-  host: process.env.DBHOST || "localhost",
-  port: process.env.DBPORT || 5432,
-  database: process.env.DBNAME || "ghosts",
-  user: process.env.DBUSER || ''
-  password: process.env.DBPASS || ''
-})
+const db = new Client(process.env.DATABASE_URL || 'localhost:5432/ghosts')
 
 db.connect()
 
