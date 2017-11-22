6cbc9af409233403a155eaa112b4482ae9146d7a
leikkisa <sallymaki@gmail.com>
2017-11-21 18:18:40 -0800

    Add findAll and findByGeo Routes

--- src/db/queries.js
+++ src/db/queries.js
@@ -3,12 +3,38 @@ const db = require('./db')
 function create(data) {
   const query = `
     INSERT INTO
-      ghosts(coords, audio, audio_title, user_id)
+      ghosts(coords, audio, audio_url, user_id, lat, lng)
     VALUES($1, $2, $3, $4)
     RETURNING id;
   `
-  return db.query(query, [ data.coords || null, data.audio || null, data.audio_title, data.user_id ])
+  return db.query(query, [ data.coords || null, data.audio || null, data.audio_url || null, data.user_id, data.lat || null, data.lng || null ])
     .then(response => response.rows[0])
+    .catch(err => { console.log(err) })
+}
+
+function findAll() {
+  const query = `
+    SELECT
+      *
+    FROM
+      ghosts;
+  `
+  return db.query(query)
+    .then(response => response.rows)
+    .catch(err => { console.log(err) })
+}
+
+function findByGeo(lat,lng) {
+  const query = `
+    SELECT
+      *
+    FROM
+      ghosts
+    WHERE round(lat,1) = round($1,1)
+    AND round(lng,1) = round($2,1);
+  `
+  return db.query(query, [lat, lng])
+    .then(response => response.rows)
     .catch(err => { console.log(err) })
 }
 
@@ -27,5 +53,7 @@ function findById(id) {
 
 module.exports = {
   create,
-  findById
+  findAll,
+  findByGeo,
+  findById,
 }
--- src/routes/marker.js
+++ src/routes/marker.js
@@ -4,17 +4,27 @@ const Query = require('../db/queries')
 const router = require('express').Router();
 
 router.get('/', (request, response) => {
-  response.send('Hello')
+  Query.findAll()
+    .then((ghosts) => response.status(200).json(ghosts))
+})
+
+router.get('/geo', (request, response) => {
+  const lat = request.query.lat
+  const lng = request.query.lng
+  Query.findByGeo(lat, lng)
+    .then((ghosts) => response.status(200).json(ghosts))
 })
 
 router.get("/:id", (request, response) => {
   const ghost_id = request.params.id
-  Query.findById(ghost_id).then((ghost) => response.status(200).send(ghost))
+  Query.findById(ghost_id)
+    .then((ghost) => response.status(200).json(ghost))
 })
 
 router.post("/", (request, response) => {
   console.log(request.body)
-  Query.create(request.body).then(ghost => response.status(200).send(ghost))
+  Query.create(request.body)
+    .then(ghost => response.status(200).json(ghost))
 })
 
 module.exports = router
